1625eca1f2ec36d64a694ce81a635f9911050c95
pomeny <hlopkov.gleb@mail.ru>
2021-04-30 19:51:49 +0300

    добавляет скрипты в build

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,25 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+
+const pageHeader = document.querySelector('.page-header');
+const headerToggle = document.querySelector('.page-header__toggle');
+const videoBikes = document.querySelector('#video-bikes');
+const videoPoster = videoBikes.querySelector('img');
+const playVideoButton = document.querySelector('#video-bikes-play');
 
 pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+headerToggle.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  if (pageHeader.classList.contains('page-header--active')) {
+    pageHeader.classList.remove('page-header--active');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('page-header--active');
   }
+});
+
+playVideoButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  videoPoster.classList.add('visually-hidden');
+  playVideoButton.classList.add('visually-hidden');
 });
